d7bb03f1d89c6fa595628c382f825c87a705efef
Ryan <ryan@war.re>
2013-08-08 03:29:32 +0000

    Added a top nav bar, just a start though

--- web/inc/template.php
+++ web/inc/template.php
@@ -7,15 +7,28 @@ abstract class Page {
   abstract function getBody();
 
   abstract function getFooter();
-  
+
+  public function getNav(){
+    //only have if there is
+    //someone logged in
+    return
+      '<div id="nav">' .
+        '<ul>' .
+          '<li><a href="/">Home</a></li>' .
+          '<li><a href="/users.php">User Info</a></li>' .
+          '<li><a href="/logout.php">Logout</a></li>' .
+        '</ul>' .
+      '</div>';
+  }
+
   final public function render(){
     $output = '<!DOCTYPE html>' .
-      '<html>' . 
+      '<html>' .
       '<head>' .
         '<link rel="stylesheet" type="text/css" href="/css/styles.css" />' .
         '<title>' .
           $this->getHeader() .
-        '</title>' . 
+        '</title>' .
       '</head>' .
       '<body>'.
         '<div id="container">' .
@@ -23,6 +36,7 @@ abstract class Page {
           $this->getHeader() .
         '</div>' .
           '<div id="body">' .
+            $this->getNav() .
             $this->getBody() .
           '</div>' .
           '<div id="footer">' .
