6cb971eeaa4c7b50be31d1aed8904508d18e8168
Ryan <ryan@war.re>
2013-08-09 06:21:36 +0000

    Added the member page template

---
+++ web/inc/member.php
@@ -0,0 +1,21 @@
+<?php
+require_once("$root/../inc/template.php");
+
+abstract class Member extends Page{
+
+  public function getNav(){
+    //only have if there is
+    //someone logged in
+    return
+      '<div id="nav">' .
+        '<ul>' .
+          '<li><a href="/">Home</a></li>' .
+          '<li><a href="/users.php">User Info</a></li>' .
+          '<li><a href="/logout.php">Logout</a></li>' .
+        '</ul>' .
+      '</div>';
+  }
+
+}
+
+?>
