5a3c16fa1087b73a058bd85d032868c026f0b5bb
Ryan Warren <ryan@war.re>
2013-12-05 07:05:20 +0000

    changed error where session id did not logout, it just changed the session cookie

--- web/root/logout.php
+++ web/root/logout.php
@@ -5,10 +5,27 @@
 //session_destroy();
 
 //@session_destroy();
-setcookie('sid', '', time()-3600);
+//setcookie('sid', '', time()-3600);
+//session_name('sid');
 session_name('sid');
+session_start();
+unset($_SESSION['userName']);
+unset($_SESSION['isAdmin']);
+//$_SESSION['userName'] = '';
+//$_SESSION['isAdmin'] = '';
+session_regenerate_id(true);
+$_SESSION = array();
+//$ses = session_id();
 session_unset();
 session_destroy();
+//session_regenerate_id();
+//$_SESSION = array();
+setcookie('sid', '', time()-3600);
+//session_start();
+//print_r($_SESSION);
+//ob_start();
+//ob_end_flush();
+session_name('sid');
 session_start();
 
 /*
@@ -36,7 +53,9 @@ setcookie('TestCookie','',time()-3600);
 //expired and session is not valid
 //TODO fix the location
 //header("Location:http://doorlock.wrixton.net/");
+
 header("Location:/");
+
 //echo "Hello <br>";
 //print_r($_COOKIE);
 //echo $_COOKIE['PHPSESSID'];
