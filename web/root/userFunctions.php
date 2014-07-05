f5bbbdb20f6dfda8349019120a612f1b3ea2bd6d
Ryan Warren <admin@wrixton.net>
2014-07-04 17:40:01 -0700

    user functions all work now

--- web/root/userFunctions.php
+++ web/root/userFunctions.php
@@ -219,7 +219,7 @@ function changeUserInfo(){
     $card = mysql_real_escape_string($_POST['card']);
     $email = mysql_real_escape_string($_POST['email']);
     $name = mysql_real_escape_string($_POST['name']);
-    print_r($_POST);
+    //print_r($_POST);
     if($newPassword !== $confNewPassword){
       echo 'new password and confirmed new password are not equal';
       return false;
