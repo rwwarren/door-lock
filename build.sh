37c742b5b741d6fe3bbc900db6571b78df20be9f
Ryan Warren <admin@wrixton.net>
2014-07-09 18:31:12 -0700

    Testing command line and travis ci

---
+++ build.sh
@@ -0,0 +1,2 @@
+mv web/inc/mysqlUserDEMO.php web/inc/mysqlUserTEST.php
+mv web/inc/variablesDEMO.php web/inc/variablesTEST.php
---
+++ web/inc/variablesTEST.php
@@ -0,0 +1,29 @@
+<?php
+
+$requiredHeaders = array("apprequest" => "lock", "UDID" => "");
+
+
+function passwordEncode($password){
+  return $password;
+}
+
+function checkAuthy(){
+
+$authy_id = '';
+
+}
+
+function checkPostVariables(){
+
+}
+
+function emailVariables(){
+  return array('name' => '', 'pass' => '');
+}
+
+function createTempPassword($user){
+  $pass = hash('sha512', "this");
+  return $pass;
+}
+
+?>
