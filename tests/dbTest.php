3b091fb144566e309eae2c9734ad9010942aa4eb
Ryan Warren <admin@wrixton.net>
2014-07-16 21:17:31 -0700

    fixed

--- tests/dbTest.php
+++ tests/dbTest.php
@@ -11,7 +11,7 @@ class DBTest extends PHPUnit_Framework_TestCase {
   //public function DBLogin() {
   public function testDBLogin() {
     $conn = new dbconn;
-    $conn->login('test', 'password');
+    //$conn->login('test', 'password');
     //include_once("/web/inc/dbcon.php");
     // Arrange
     //$a = new Money(1);
