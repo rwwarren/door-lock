6fbfebf2f1879ddab001188c782af64c17ddd076
Ryan Warren <admin@wrixton.net>
2014-07-19 15:43:45 -0700

    db test skeletons added

--- tests/dbTest.php
+++ tests/dbTest.php
@@ -5,25 +5,199 @@ include_once("web/inc/dbcon.php");
 
 class DBTest extends PHPUnit_Framework_TestCase {
 
-  /*
+  /**
+   * @test
+   * @expectedException InvalidArgumentException
+   * @expectedExceptionMessage Database user not selected properly
+   */
+  public function createInvalidDBObject() {
+    $conn = new dbconn("reader");
+  }
+
+  /**
    * @test
    */
-  //public function DBLogin() {
-  public function testDBLogin() {
+  public function createValidDBObject() {
+    $conn = new dbconn("read");
+  }
+
+  /**
+   * @test
+   */
+  public function deleteValidDBObject() {
+    $conn = new dbconn("read");
+    unset($conn);
+    $this->assertTrue(!isset($conn));
+  }
+
+  /**
+   * @test
+   */
+  public function validLogin() {
     //ob_start();
     $conn = new dbconn("read");
     //$conn->connect("read");
     //$results = $conn->login('test', 'password');
     //$this->assertContains('HTTP/1.0 403 Error Username or Password incorrect', $results);
     //include_once("/web/inc/dbcon.php");
-    // Arrange
-    //$a = new Money(1);
+  }
+  /**
+   * @test
+   */
+  public function invalidLogin() {
+    $conn = new dbconn("read");
+  }
 
-    // Act
-    //$b = $a->negate();
+  /**
+   * @test
+   */
+  public function validCheckAuthy() {
+    //
+  }
 
-    // Assert
-    //$this->assertEquals(-1, $b->getAmount());
+  /**
+   * @test
+   */
+  public function invalidCheckAuthy() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getActiveUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getInactiveUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getAdmins() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function validChangePassword() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function invalidChangePassword() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function updateUserInfo() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function checkPassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetChangePassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function registerUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function removeUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function changeUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetPassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function findResetToken() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function findUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetPasswordQuery() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function invalidateResetURL() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getUserInfo() {
+    //TODO valid and invalid testing
+    //
   }
 
 }
