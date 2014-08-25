89d7126526ef8eb3e1d4c06d410a5bc3f48629f6
Ryan <ryan@war.re>
2014-08-24 23:28:49 -0700

    added tests back

---
+++ web/tests/dbTest.php
@@ -0,0 +1,208 @@
+<?php
+//include_once("../web/inc/dbcon.php");
+include_once("web/inc/dbcon.php");
+//include "../web/inc/variables..php";
+
+class DBTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * @test
+   * @expectedException InvalidArgumentException
+   * @expectedExceptionMessage Database user not selected properly
+   */
+  public function createInvalidDBObject() {
+    $conn = new dbconn("reader");
+  }
+
+  /**
+   * @test
+   */
+  public function createValidDBObject() {
+    $conn = new dbconn("read");
+  }
+
+  /**
+   * @test
+   */
+  public function deleteValidDBObject() {
+    $conn = new dbconn("read");
+    unset($conn);
+    $this->assertTrue(!isset($conn));
+  }
+
+  /**
+   * @test
+   * @runInSeparateProcess
+   */
+  public function validLogin() {
+    //ob_start();
+    $conn = new dbconn("read");
+    //$conn->connect("read");
+    //$results = $conn->login('test', 'test');
+    //$this->assertContains('HTTP/1.0 403 Error Username or Password incorrect', $results);
+    //include_once("/web/inc/dbcon.php");
+  }
+  /**
+   * @test
+   */
+  public function invalidLogin() {
+    $conn = new dbconn("read");
+    //$results = $conn->login('test', 'password');
+  }
+
+  /**
+   * @test
+   */
+  public function validCheckAuthy() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function invalidCheckAuthy() {
+    $conn = new dbconn("read");
+    //$invalidAuthy = $conn->checkAuthy("test", "asdf");
+    //$this->assertFalse($invalidAuthy);
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getActiveUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getInactiveUsers() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getAdmins() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function validChangePassword() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function invalidChangePassword() {
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function updateUserInfo() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function checkPassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetChangePassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function registerUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function removeUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function changeUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetPassword() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function findResetToken() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function findUser() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function resetPasswordQuery() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function invalidateResetURL() {
+    //TODO valid and invalid testing
+    //
+  }
+
+  /**
+   * @test
+   */
+  public function getUserInfo() {
+    //TODO valid and invalid testing
+    //
+  }
+
+}
---
+++ web/tests/userFunctionTest.php
@@ -0,0 +1,13 @@
+<?php
+//include_once("web/root/userFuncations.php");
+
+class userFunctionTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * @test
+   */
+  public function login() {
+
+  }
+
+}
