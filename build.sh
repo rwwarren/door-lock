3a0d48f88f59789138bce37136da72ce6e6461d9
Ryan <ryan@war.re>
2014-08-25 22:30:57 -0700

    testing adding new bash arg

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mv web/inc/mysqlUserTEST.php web/inc/mysqlUser.php
 mv web/inc/variablesTEST.php web/inc/variables.php
 mv web/coveralls.yml web/.coveralls.yml
-$DIR/build/build.sh
+PASSED_IN_ARG=$1
+if [[ $PASSED_IN_ARG == "install" ]]
+  $DIR/build/build.sh
+fi
