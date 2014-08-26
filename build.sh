de7866f6126becfb4447bdd2465b6c416fed9de0
Ryan <ryan@war.re>
2014-08-26 00:16:22 -0700

    build changes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mv web/inc/mysqlUserTEST.php web/inc/mysqlUser.php
 mv web/inc/variablesTEST.php web/inc/variables.php
 mv web/coveralls.yml web/.coveralls.yml
 PASSED_IN_ARG=$1
-if [[ $PASSED_IN_ARG == $TYPE ]]
-then
-  $DIR/build/build.sh
-fi
+#if [[ $PASSED_IN_ARG == $TYPE ]]
+#then
+#  $DIR/build/build.sh
+#fi
