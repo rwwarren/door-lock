8738692830828d8bb1031b13a77214f363466220
Ryan Warren <ryan@war.re>
2015-01-14 11:48:26 -0800

    try fix travisci

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ TYPE="install"
 mv web/inc/mysqlUserTEST.php web/inc/mysqlUser.php
 mv web/inc/variablesTEST.php web/inc/variables.php
 mv web/src/coveralls.yml web/src/.coveralls.yml
+mkdir web/src/properties
+cp web/build/example.properties web/src/properties/secure.ini
 PASSED_IN_ARG=$1
 #if [[ $PASSED_IN_ARG == $TYPE ]]
 #then
