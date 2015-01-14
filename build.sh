3e224157ea559cf73bc498ec159b047b959269f8
Ryan Warren <ryan@war.re>
2015-01-14 00:02:44 -0800

    travis-ci fixes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ TYPE="install"
 
 mv web/inc/mysqlUserTEST.php web/inc/mysqlUser.php
 mv web/inc/variablesTEST.php web/inc/variables.php
-mv web/coveralls.yml web/.coveralls.yml
+mv web/src/coveralls.yml web/src/.coveralls.yml
 PASSED_IN_ARG=$1
 #if [[ $PASSED_IN_ARG == $TYPE ]]
 #then
