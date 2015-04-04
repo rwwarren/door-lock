e74cd356bfd638fca3df4ba6683db99dc058ded4
Ryan Warren <ryan@war.re>
2015-04-04 15:45:06 -0700

    touchable things

--- app/doorlock-app/index.ios.js
+++ app/doorlock-app/index.ios.js
@@ -13,14 +13,32 @@ var {
   ListView,
   NavigatorIOS,
   Navigator,
+  ScrollView,
+  NavButton,
 } = React;
 
 var HomePage = require('./HomePage');
 
 var doorlockapp = React.createClass({
+  getInitialState: function() {
+      return {
+        isLoggedIn: false,
+      }
+  },
   render: function() {
-    //if (!this.state.loaded) {
-    //  return <Text>asdf</Text>;
+    if (!this.state.isLoggedIn) {
+      return this.returnLoginPage();
+      //return (
+      //  <View style={styles.container}>
+      //    <Text>asdf her</Text>
+      //  </View>
+      //);
+    }
+      return (
+        <View style={styles.container}>
+          <Text>Please Login</Text>
+        </View>
+      );
     //}
 //      <NavigatorIOS
 //        style={styles.container}
@@ -28,34 +46,120 @@ var doorlockapp = React.createClass({
 //          title: 'Home Page',
 //          component: HomePage,
 //      }}/>
-    return(
-<Navigator
-    initialRoute={{name: 'My First Scene', index: 0}}
-    renderScene={(route, navigator) =>
-      <HomePage
-        name={route.name}
-        onForward={() => {
-          var nextIndex = route.index + 1;
-          navigator.push({
-            name: 'Scene ' + nextIndex,
-            index: nextIndex,
-          });
-        }}
-        onBack={() => {
-          if (route.index > 0) {
-            navigator.pop();
-          }
-        }}
-      />
-    }
-  />
 
-     );
+//    return (
+//      <Navigator
+//        style={styles.container}
+//        initialRoute={{name: 'My First Scene', index: 0}}
+//        renderScene={(route, navigator) =>
+//          <HomePage
+//            name={route.name}
+//            onForward={() => {
+//              var nextIndex = route.index + 1;
+//              navigator.push({
+//                name: 'Scene ' + nextIndex,
+//                index: nextIndex,
+//              });
+//            }}
+//            onBack={() => {
+//              if (route.index > 0) {
+//                navigator.pop();
+//              }
+//            }}
+//          />
+//        }
+//      />
+//    );
+
+//    return (
+//      <Navigator
+//        style={styles.container}
+//        initialRoute={this.getRandomRoute()}
+//        renderScene={this.renderScenes}
+//        navigationBar={
+//          <Navigator.BreadcrumbNavigationBar
+//            routeMapper={this.navBarRouteMapper}
+//          />
+//        }
+//      />
+//    );
+
+//    return (
+//      <View style={styles.container}>
+//        <Text>asdf</Text>
+//      </View>
+//    );
   },
+//  getRandomRoute: function() {
+//    return {
+//      title: '#' + Math.ceil(Math.random() * 1000),
+//    };
+//  },
+//  renderScenes: function(route, navigator) {
+//    return (
+//      <ScrollView style={styles.scene}>
+//        <NavButton
+//          onPress={() => { navigator.push(this.getRandomRoute()) }}
+//          text="Push"
+//        />
+//        <NavButton
+//          onPress={() => { navigator.immediatelyResetRouteStack([this.getRandomRoute(), this.getRandomRoute()]) }}
+//          text="Reset w/ 2 scenes"
+//        />
+//        <NavButton
+//          onPress={() => { navigator.popToTop() }}
+//          text="Pop to top"
+//        />
+//        <NavButton
+//          onPress={() => { navigator.replace(this.getRandomRoute()) }}
+//          text="Replace"
+//        />
+//        <NavButton
+//          onPress={() => { this.props.navigator.pop(); }}
+//          text="Close breadcrumb example"
+//        />
+//      </ScrollView>
+//    );
+//  },
+//  componentWillMount: function() {
+//    this.navBarRouteMapper = {
+//      rightContentForRoute: function(route, navigator) {
+//        return null;
+//      },
+//      titleContentForRoute: function(route, navigator) {
+//        return (
+//          <TouchableOpacity
+//            onPress={() => navigator.push(this.getRandomRoute())}>
+//            <View>
+//              <Text style={styles.titleText}>{route.title}</Text>
+//            </View>
+//          </TouchableOpacity>
+//        );
+//      },
+//      iconForRoute: function(route, navigator) {
+//        return (
+//          <TouchableOpacity onPress={() => {
+//            navigator.popToRoute(route);
+//          }}>
+//            <View style={styles.crumbIconPlaceholder} />
+//          </TouchableOpacity>
+//        );
+//      },
+//      separatorForRoute: function(route, navigator) {
+//        return (
+//          <TouchableOpacity onPress={navigator.pop}>
+//            <View style={styles.crumbSeparatorPlaceholder} />
+//          </TouchableOpacity>
+//        );
+//      }
+//    };
+//  },
 });
 
 var styles = StyleSheet.create({
   container: {
+  //  marginTop: 65,
+    marginTop: 25,
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
@@ -83,6 +187,41 @@ var styles = StyleSheet.create({
     textAlign: 'left',
     left: 15,
   },
+ scene: {
+    paddingTop: 50,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderBottomWidth: 1 / 10,
+    borderBottomColor: '#CDCDCD',
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  //container: {
+  //  overflow: 'hidden',
+  //  backgroundColor: '#dddddd',
+  //  flex: 1,
+  //},
+  titleText: {
+    fontSize: 18,
+    color: '#666666',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    lineHeight: 32,
+  },
+  crumbIconPlaceholder: {
+    flex: 1,
+    backgroundColor: '#666666',
+  },
+  crumbSeparatorPlaceholder: {
+    flex: 1,
+    backgroundColor: '#aaaaaa',
+  },
 });
 
 AppRegistry.registerComponent('doorlock-app', () => doorlockapp);
+
