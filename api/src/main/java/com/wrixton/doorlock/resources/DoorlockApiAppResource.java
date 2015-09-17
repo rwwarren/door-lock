e312aec293cde838dab00198972700f02fc94236
Ryan Warren <ryan@war.re>
2015-09-16 23:59:51 -0700

    Added some queries

--- api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
+++ api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
@@ -1,16 +1,49 @@
 package com.wrixton.doorlock.resources;
 
+import com.codahale.metrics.annotation.Timed;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class DoorlockApiAppResource {
 
     @GET
-    public String testing(){
-        return "this is a sick test!";
+    @Timed
+    @Produces(MediaType.TEXT_HTML)
+    public InputStream getIndex() {
+        HttpClient client = new DefaultHttpClient();
+        try {
+            HttpGet request = new HttpGet("http://docs.doorlock.apiary.io/");
+            HttpResponse response = client.execute(request);
+            return response.getEntity().getContent();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println(e.getStackTrace().toString());
+        }
+        return null;
     }
+
+    @POST
+    @Timed
+    @Path("/login")
+    public String login(String body){
+//    public Login login(String body){
+        return null;
+    }
+
 }
