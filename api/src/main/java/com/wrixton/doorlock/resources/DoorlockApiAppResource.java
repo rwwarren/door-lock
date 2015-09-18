30a39ac47b50502c6590eb12d89195eb2e7942ab
Ryan Warren <ryan@war.re>
2015-09-17 19:13:06 -0700

    new rest client

--- api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
+++ api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
@@ -1,21 +1,16 @@
 package com.wrixton.doorlock.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.ws.rs.core.Response;
 
 
 @Path("/")
@@ -25,23 +20,16 @@ public class DoorlockApiAppResource {
     @GET
     @Timed
     @Produces(MediaType.TEXT_HTML)
-    public InputStream getIndex() {
-        HttpClient client = new DefaultHttpClient();
-        try {
-            HttpGet request = new HttpGet("http://docs.doorlock.apiary.io/");
-            HttpResponse response = client.execute(request);
-            return response.getEntity().getContent();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            System.out.println(e.getStackTrace().toString());
-        }
-        return null;
+    public Response getIndex() {
+        Client myClient = ClientBuilder.newClient();
+        WebTarget target = myClient.target("http://docs.doorlock.apiary.io/");
+        return target.request(MediaType.TEXT_HTML).get();
     }
 
     @POST
     @Timed
     @Path("/login")
-    public String login(String body){
+    public String login(String body) {
 //    public Login login(String body){
         return null;
     }
