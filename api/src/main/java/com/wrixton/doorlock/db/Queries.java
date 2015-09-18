d481b9485491a4d62c13434b0e39106933557cc9
Ryan Warren <ryan@war.re>
2015-09-18 08:37:45 -0700

    a little cleanup

--- api/src/main/java/com/wrixton/doorlock/db/Queries.java
+++ api/src/main/java/com/wrixton/doorlock/db/Queries.java
@@ -14,20 +14,12 @@ public class Queries {
 
     public Queries() throws Exception {
         try {
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/doorlock");
 //            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/doorlock?user=read&password=PASSWORD");
 //            conn = DriverManager.getConnection("jdbc:mysql://localhost/doorlock?user=write&password=PASSWORD");
-
             String user = "read";
             String pass = "PASSWORD";
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/doorlock",
                     user, pass);
-            // Do something with the Connection
-//        } catch (SQLException ex) {
-            // handle any errors
-//            System.out.println("SQLException: " + ex.getMessage());
-//            System.out.println("SQLState: " + ex.getSQLState());
-//            System.out.println("VendorError: " + ex.getErrorCode());
         } catch (Exception e) {
             System.out.println(e);
             throw e;
