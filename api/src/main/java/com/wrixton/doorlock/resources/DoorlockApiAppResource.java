2fbace69ebb3ffe21574b78c1b718cef90e9332a
Ryan Warren <ryan@war.re>
2015-09-20 14:54:07 -0700

    use java 1.8

--- api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
+++ api/src/main/java/com/wrixton/doorlock/resources/DoorlockApiAppResource.java
@@ -6,12 +6,16 @@ import com.wrixton.doorlock.LoginRequest;
 import com.wrixton.doorlock.db.Queries;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
 
 
 @Path("/")
@@ -35,6 +39,7 @@ public class DoorlockApiAppResource {
         try {
             Queries queries = new Queries();
             body.getSid();
+            System.out.println(body.getSid());
             return queries.login(body.getUsername(), body.getPassword());
         } catch (Exception e) {
             e.printStackTrace();
