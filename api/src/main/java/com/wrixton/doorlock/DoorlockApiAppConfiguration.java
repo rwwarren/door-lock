42b2ce1516af7aa2d08b73c37dd3b6add882bc67
Ryan Warren <ryan@war.re>
2015-11-29 00:43:41 -0800

    fix and update pom

--- api/src/main/java/com/wrixton/doorlock/DoorlockApiAppConfiguration.java
+++ api/src/main/java/com/wrixton/doorlock/DoorlockApiAppConfiguration.java
@@ -3,7 +3,9 @@ package com.wrixton.doorlock;
 import com.bendb.dropwizard.redis.JedisFactory;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+//import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.db.PooledDataSourceFactory;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
@@ -35,7 +37,7 @@ public class DoorlockApiAppConfiguration extends Configuration {
     @JsonProperty("database")
     private DataSourceFactory database = new DataSourceFactory();
 
-    public DataSourceFactory getDataSourceFactory() {
+    public PooledDataSourceFactory getDataSourceFactory() {
         return database;
     }
 
