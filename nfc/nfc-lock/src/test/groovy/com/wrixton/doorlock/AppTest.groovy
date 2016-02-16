73fd4718eb415e1ee98fce31716c3bf7b568e3d1
Ryan Warren <rwwarren@users.noreply.github.com>
2016-02-15 17:49:15 -0800

    cruft cleaning

--- nfc/nfc-lock/src/test/groovy/com/wrixton/doorlock/AppTest.groovy
+++ nfc/nfc-lock/src/test/groovy/com/wrixton/doorlock/AppTest.groovy
@@ -3,32 +3,7 @@ package com.wrixton.doorlock
 import groovyx.net.http.HttpResponseException
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-//    public AppTest( String testName ) {
-//        super( testName );
-//    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-//    public static Test suite() {
-//        return new TestSuite( AppTest.class );
-//    }
-
-    /**
-     * Rigourous Test :-)
-     */
-//    public void testApp() {
-//        assertTrue( true );
-//    }
 
     @Test(expected = HttpResponseException.class)
     public void testChangeLockInvalidUID(){
